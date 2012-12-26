ab2a2afd0920a1c05924911c89d96efc1643553f
YuQing Zhang <zhangyuqing524@gmail.com>
2012-12-27 00:08:40 +0100

    Added unit tests for OpenLibrary
    Added unit tests for OpenLibrary
    Refactor the code of OpenLibrary

---
+++ Shared/OpenLibrary.UnitTests/DetailsUnitTests.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OpenLibrary.UnitTests
+{
+    [TestClass]
+    public class DetailsUnitTests
+    {
+        private Details sut = new Details();
+
+        [TestMethod]
+        public void FindOLIDsByTitle_WithValidTitle_ReturnsOLIDs()
+        {
+            string title = "Python Essential Reference";
+
+            List<string> actual = sut.FindOLIDsByTitle(title);
+
+            Assert.AreEqual(4, actual.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindOLIDsByTitle_WithEmptyTitle_ShouldThrowArgumentNullException()
+        {
+
+            List<string> actual = sut.FindOLIDsByTitle(string.Empty);
+        }
+
+        [TestMethod]
+        public void FindOLIDsByTitle_WithNonExistingTitle_ReturnsEmpty()
+        {
+            string title = "Yuqing Zhang";
+
+            List<string> actual = sut.FindOLIDsByTitle(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public void FindOLIDsByTitle_WithInvalidTitle_ReturnsEmpty()
+        {
+            string title = "Python Essential Reference\"";
+
+            List<string> actual = sut.FindOLIDsByTitle(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public void FindBooksByTitle_WithValidTitle_ReturnsBooks()
+        {
+            string title = "Python Essential Reference";
+
+            List<BookData> actual = sut.FindBooksByTitle(title);
+
+            Assert.AreEqual(4, actual.Count);
+            foreach (var book in actual)
+            {
+                Assert.IsTrue(book.Title.Contains(title));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindBooksByTitle_WithEmptyTitle_ShouldThrowArgumentNullException()
+        {
+
+            List<BookData> actual = sut.FindBooksByTitle(string.Empty);
+        }
+
+        [TestMethod]
+        public void FindBooksByTitle_WithNonExistingTitle_ReturnsEmpty()
+        {
+            string title = "Yuqing Zhang";
+
+            List<BookData> actual = sut.FindBooksByTitle(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public void FindBooksByTitle_WithInvalidTitle_ReturnsEmpty()
+        {
+            string title = "Python Essential Reference\"";
+
+            List<BookData> actual = sut.FindBooksByTitle(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public void FindBooksByOLIDs_WithValidOLIDs_ReturnsBooks()
+        {
+            List<string> oLIDs = new List<string>()
+            {
+                "OL24778132M",
+                "OL24652845M",
+            };
+
+            List<string> oLIDsWithOneInvalidOLID = oLIDs;
+            oLIDsWithOneInvalidOLID.Add("123XX");
+            List<BookData> actual = sut.FindBooksByOLIDs(oLIDsWithOneInvalidOLID);
+
+            Assert.AreEqual(2, actual.Count);
+            foreach (var book in actual)
+            {
+                Assert.IsTrue(oLIDs.Contains(book.Identifiers.OpenLibrary[0]));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindBooksByOLIDs_WithNullOLIDs_ShouldThrowArgumentNullException()
+        {
+
+            List<BookData> actual = sut.FindBooksByOLIDs(null);
+        }
+
+        [TestMethod]
+        public async Task FindOLIDsByTitleAsync_WithValidTitle_ReturnsOLIDs()
+        {
+            string title = "Python Essential Reference";
+
+            List<string> actual = await sut.FindOLIDsByTitleAsync(title);
+
+            Assert.AreEqual(4, actual.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task FindOLIDsByTitleAsync_WithEmptyTitle_ShouldThrowArgumentNullException()
+        {
+
+            List<string> actual = await sut.FindOLIDsByTitleAsync(string.Empty);
+        }
+
+        [TestMethod]
+        public async Task FindOLIDsByTitleAsync_WithNonExistingTitle_ReturnsEmpty()
+        {
+            string title = "Yuqing Zhang";
+
+            List<string> actual = await sut.FindOLIDsByTitleAsync(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public async Task FindOLIDsByTitleAsync_WithInvalidTitle_ReturnsEmpty()
+        {
+            string title = "Python Essential Reference\"";
+
+            List<string> actual = await sut.FindOLIDsByTitleAsync(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public async Task FindBooksByTitleAsync_WithValidTitle_ReturnsBooks()
+        {
+            string title = "Python Essential Reference";
+
+            List<BookData> actual = await sut.FindBooksByTitleAsync(title);
+
+            Assert.AreEqual(4, actual.Count);
+            foreach (var book in actual)
+            {
+                Assert.IsTrue(book.Title.Contains(title));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task FindBooksByTitleAsync_WithEmptyTitle_ShouldThrowArgumentNullException()
+        {
+
+            List<BookData> actual = await sut.FindBooksByTitleAsync(string.Empty);
+        }
+
+        [TestMethod]
+        public async Task FindBooksByTitleAsync_WithNonExistingTitle_ReturnsEmpty()
+        {
+            string title = "Yuqing Zhang";
+
+            List<BookData> actual = await sut.FindBooksByTitleAsync(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public async Task FindBooksByTitleAsync_WithInvalidTitle_ReturnsEmpty()
+        {
+            string title = "Python Essential Reference\"";
+
+            List<BookData> actual = await sut.FindBooksByTitleAsync(title);
+
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [TestMethod]
+        public async Task FindBooksByOLIDsAsync_WithValidOLIDs_ReturnsBooks()
+        {
+            List<string> oLIDs = new List<string>()
+            {
+                "OL24778132M",
+                "OL24652845M",
+            };
+
+            List<string> oLIDsWithOneInvalidOLID = oLIDs;
+            oLIDsWithOneInvalidOLID.Add("123XX");
+            List<BookData> actual = await sut.FindBooksByOLIDsAsync(oLIDsWithOneInvalidOLID);
+
+            Assert.AreEqual(2, actual.Count);
+            foreach (var book in actual)
+            {
+                Assert.IsTrue(oLIDs.Contains(book.Identifiers.OpenLibrary[0]));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task FindBooksByOLIDsAsync_WithNullOLIDs_ShouldThrowArgumentNullException()
+        {
+
+            List<BookData> actual = await sut.FindBooksByOLIDsAsync(null);
+        }
+    }
+}
--- Shared/OpenLibrary/Details.cs
+++ Shared/OpenLibrary/Details.cs
@@ -19,13 +19,9 @@ namespace OpenLibrary
             {
                 throw new ArgumentNullException("title");
             }
-            List<BookData> books = null;
+            List<BookData> books = new List<BookData>();
 
-            // OpenLibrary is friendly with the book title whose first character is captial. 
-            title = title.Replace(title[0], title.ToUpper()[0]);
-
-            var uri = baseUrl + "things?query={\"type\":\"\\/type\\/edition\",\"title~\":\"" + title + "*\"}&prettyprint=true&text=true";
-            var oLIDs = this.GetOLIDs(uri);
+            var oLIDs = this.FindOLIDsByTitle(title);
 
             if (oLIDs != null)
             {
@@ -41,17 +37,13 @@ namespace OpenLibrary
                 throw new ArgumentNullException("oLIDs");
             }
 
-            List<BookData> books = null;
+            List<BookData> books = new List<BookData>();
 
             var bibkeys = new StringBuilder();
-            foreach (var oLId in oLIDs)
+            foreach (var oLID in oLIDs)
             {
-                string strToRemove = "/books/";
-                if (oLId.StartsWith(strToRemove))
-                {
-                    bibkeys.Append(oLId.Replace(strToRemove, ""));
-                    bibkeys.Append(",");
-                }
+                bibkeys.Append(oLID);
+                bibkeys.Append(",");
             }
             if (bibkeys.Length != 0)
             {
@@ -73,20 +65,32 @@ namespace OpenLibrary
             return books;
         }
 
-        public List<string> GetOLIDs(string requestUri)
+        public List<string> FindOLIDsByTitle(string title)
         {
-            if (string.IsNullOrEmpty(requestUri))
+            if (string.IsNullOrEmpty(title))
             {
-                throw new ArgumentNullException(requestUri);
+                throw new ArgumentNullException("title");
             }
 
-            List<string> oLIDs = null;
+            // OpenLibrary is friendly with the book title whose first character is captial. 
+            title = title.Replace(title[0], title.ToUpper()[0]);
+
+            var uri = baseUrl + "things?query={\"type\":\"\\/type\\/edition\",\"title~\":\"" + title + "*\"}&prettyprint=true&text=true";
+
+            List<string> oLIDs = new List<string>();
             using (var webClient = new WebClient())
             {
-                var response = JsonConvert.DeserializeObject<Thing>(webClient.DownloadString(requestUri));
+                var response = JsonConvert.DeserializeObject<Thing>(webClient.DownloadString(uri));
                 if (response.Status.Equals("ok", StringComparison.OrdinalIgnoreCase))
                 {
-                    oLIDs = response.Result;
+                    foreach (var oLID in response.Result)
+                    {
+                        string strToRemove = "/books/";
+                        if (oLID.StartsWith(strToRemove))
+                        {
+                            oLIDs.Add(oLID.Replace(strToRemove, ""));
+                        }
+                    }
                 }
             }
             return oLIDs;
@@ -98,13 +102,9 @@ namespace OpenLibrary
             {
                 throw new ArgumentNullException("title");
             }
-            List<BookData> books = null;
+            List<BookData> books = new List<BookData>();
 
-            // OpenLibrary is friendly with the book title whose first character is captial. 
-            title = title.Replace(title[0], title.ToUpper()[0]);
-
-            var uri = baseUrl + "things?query={\"type\":\"\\/type\\/edition\",\"title~\":\"" + title + "*\"}&prettyprint=true&text=true";
-            var oLIDs = await this.GetOLIDsAsync(uri);
+            var oLIDs = await this.FindOLIDsByTitleAsync(title);
 
             if (oLIDs != null)
             {
@@ -120,17 +120,13 @@ namespace OpenLibrary
                 throw new ArgumentNullException("oLIDs");
             }
 
-            List<BookData> books = null;
+            List<BookData> books = new List<BookData>();
 
             var bibkeys = new StringBuilder();
-            foreach (var oLId in oLIDs)
+            foreach (var oLID in oLIDs)
             {
-                string strToRemove = "/books/";
-                if (oLId.StartsWith(strToRemove))
-                {
-                    bibkeys.Append(oLId.Replace(strToRemove, ""));
-                    bibkeys.Append(",");
-                }
+                bibkeys.Append(oLID);
+                bibkeys.Append(",");
             }
             if (bibkeys.Length != 0)
             {
@@ -139,7 +135,7 @@ namespace OpenLibrary
                 using (var httpClient = new HttpClient())
                 {
                     var response = await httpClient.GetAsync(getUri);
-                    var bookDetails = await response.Content.ReadAsAsync<Dictionary<string, BookData>>();
+                    var bookDetails = JsonConvert.DeserializeObject<Dictionary<string, BookData>>(await response.Content.ReadAsStringAsync());
                     if (bookDetails != null && bookDetails.Values.Count() > 0)
                     {
                         books = new List<BookData>();
@@ -153,20 +149,32 @@ namespace OpenLibrary
             return books;
         }
 
-        public async Task<List<string>> GetOLIDsAsync(string requestUri)
+        public async Task<List<string>> FindOLIDsByTitleAsync(string title)
         {
-            if (string.IsNullOrEmpty(requestUri))
+            if (string.IsNullOrEmpty(title))
             {
-                throw new ArgumentNullException(requestUri);
+                throw new ArgumentNullException("title");
             }
-            List<string> oLIDs = null;
+
+            // OpenLibrary is friendly with the book title whose first character is captial. 
+            title = title.Replace(title[0], title.ToUpper()[0]);
+            var uri = baseUrl + "things?query={\"type\":\"\\/type\\/edition\",\"title~\":\"" + title + "*\"}&prettyprint=true&text=true";
+            
+            List<string> oLIDs = new List<string>();
             using (var httpClient = new HttpClient())
             {
-                var response = await httpClient.GetAsync(requestUri);
-                var thing = await response.Content.ReadAsAsync<Thing>();
+                var response = await httpClient.GetAsync(uri);
+                var thing = JsonConvert.DeserializeObject<Thing>(await response.Content.ReadAsStringAsync());
                 if (thing.Status.Equals("ok", StringComparison.OrdinalIgnoreCase))
                 {
-                    oLIDs = thing.Result;
+                    foreach (var oLID in thing.Result)
+                    {
+                        string strToRemove = "/books/";
+                        if (oLID.StartsWith(strToRemove))
+                        {
+                            oLIDs.Add(oLID.Replace(strToRemove, ""));
+                        }
+                    }
                 }
             }
             return oLIDs;
